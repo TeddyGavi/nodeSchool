bc0d543f8bc86beda9aada2bedc35b8952a6c727
teddygavi <matc.davis@gmail.com>
2023-04-02 00:20:51 -0600

    set up basic server to beign exercise

--- http-file-server.js
+++ http-file-server.js
@@ -1,1 +1,7 @@
 const http = require("http");
+const port = +process.argv[2];
+const fileLoc = process.argv[3];
+
+const server = http.createServer((req, res) => {});
+
+server.listen(port, () => console.log(`listening on port ${port}`));
