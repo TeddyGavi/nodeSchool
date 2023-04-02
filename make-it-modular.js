715119d2027d1fef39d53fcca86b022e48abc7e3
teddygavi <matc.davis@gmail.com>
2023-04-01 22:04:39 -0600

    finished modular

--- make-it-modular.js
+++ make-it-modular.js
@@ -2,13 +2,11 @@ const mymodule = require("./mymodule")
 const file = process.argv[2]
 const ext =  process.argv[3]
 
-mymodule((err, data) => {
+mymodule(file, ext, (err, data) => {
   if (err) {
   return console.log(err)
   } 
-    for (const item of data) {
-      console.log(item)
-    }
+  data.forEach(item => console.log(item))
 
   
 })
